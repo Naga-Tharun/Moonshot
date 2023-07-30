c959e49d7a99234a2e386ce3b61e74fb85a23cb9
Naga-Tharun <nagatharunmakkena@gmail.com>
2023-07-30 17:13:06 +0530

    Adding Files

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,15 +7,60 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            MissionView(mission: mission, astronauts: astronauts)
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+                .preferredColorScheme(.dark)
+            }
+            .navigationTitle("MoonShot")
+            .background(.darkBackground)  
         }
-        .padding()
     }
 }
 
